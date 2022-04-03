1929470d233e50635b3b1f52bf8234460f3fc2b6
imyashsingh <yash.singh5445@gmail.com>
2022-04-03 16:02:41 +0530

    Merge Intervals

---
+++ array/Merge Intervals.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/merge-intervals/
+//Merge Intervals
+
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        int start=intervals[0][0],n=intervals.size(),end=intervals[0][1];
+        for(int i=1;i<n;i++){
+            if(end>=intervals[i][0])
+                end=max(end,intervals[i][1]);
+            else{
+                ans.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+            }
+        } 
+        ans.push_back({start,end});
+        return ans;
+    }
