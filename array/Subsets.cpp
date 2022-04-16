fc5670bf74aa55ae5d269c8acf1d4379739f5d4d
imyashsingh <yash.singh5445@gmail.com>
2022-04-16 17:09:40 +0530

    Subsets

---
+++ array/Subsets.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/subsets/
+// Subsets
+
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        int len=1<<n;
+        for(int i=0;i<len;i++){
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j))
+                    temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
