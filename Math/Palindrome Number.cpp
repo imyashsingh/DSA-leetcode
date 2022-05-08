506dfc66d42f14ad4af80cf5c38379461f884523
imyashsingh <yash.singh5445@gmail.com>
2022-05-08 15:20:50 +0530

    Palindrome Number

---
+++ Math/Palindrome Number.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/palindrome-number/
+// Palindrome Number
+
+
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+            return false;
+        string s=to_string(x);
+        int n=s.size();
+        for(int i=0;i<n/2;i++){
+            if(s[i] != s[n-1-i])
+                return false;
+        }
+        return true;
+    }
+};
